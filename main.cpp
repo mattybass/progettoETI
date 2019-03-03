54769f20b8fd90b846eff3726318081b643addcd
Unknown <daniele.mattedi@studenti.unitn.it>
2019-03-03 16:18:19 +0100

    aggiunta lettura json

--- main.cpp
+++ main.cpp
@@ -3,17 +3,19 @@
 #include "coordinata.h"
 #include "angolo.h"
 #include "utility.h"
+#include "persona.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
 
 int main(int argc, char** argv) {
-
+	/*
 	Coordinata B(0,0,0);
 	Coordinata A(9,4,0);
 	Coordinata C(-2,4,0);
 	double differenza=angolo_azimut(A,B,C);
-	cout<<differenza;
+	cout<<differenza;*/
+	Persona p("spallagomitoModello.json");
 	return 0;
 }
 
