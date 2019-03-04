84400ebcb48c100295403706b8042c9451ccf448
Unknown <sergio.povoli@studenti.unitn.it>
2019-03-04 13:25:53 +0100

    Popola framedaanalizzare finito

--- persona.h
+++ persona.h
@@ -20,20 +20,20 @@ class Persona{
 private:
     map<int,Frame> sequenzaframe; //Contiene tutti i frame ordinati tramite una chiave: il numero progressivo.
     map<string,list<Angolo>> sequenzaangolo; //MAP con chaive nome dell'angolo e argomento lista di Angolo. Rappresenta l'andamento temporale dei vari angolii. [Variazione nel empo: NB siamo nel discreto]
-    map<string,set<int>> max_min_angoli_zenit; //MAP con chiave nome dell'angolo e argomento una set di massimi e minimi relativi che si discostano di una percentuale x dalla media dell'angolo.
-    map<string,set<int>> max_min_angoli_azimut;
-    set<int> framedaanalizzare;
-    map<string,set<int>> framexangolo;
+    map<string,set<int>> max_min_angoli_zenit; //MAP con chiave nome dell'angolo e argomento una set di massimi e minimi relativi che si discostano di una percentuale x dalla media dell'angolo. Per gli angoli Zenit
+    map<string,set<int>> max_min_angoli_azimut;//MAP con chiave nome dell'angolo e argomento una set di massimi e minimi relativi che si discostano di una percentuale x dalla media dell'angolo. Per gli angoli Azimut
+    set<int> framedaanalizzare;//Unione di tutti i frame da analizzare
     
-    
+    //Funzioni
     void popola_max_min_angolo_zenit(string _angolo, float _tolleranzazenit); //Popola la lista di max min di UN SINGOLO ANGOLO con una tolleranzza specifica.
+    void popola_max_min_angolo_azimut(string _angolo, float _tolleranzaazimut); //Popola la lista di max min di UN SINGOLO ANGOLO con una tolleranzza specifica.
 public:
 	Persona(string nome_file);
 	void insert_frame(int _numeroframe); //Crea un frame vuoto e lo inserisce nella map.
     void insert_coordinata(int _numeroframe,int _numerojoint,double _x, double _y, double _z); //Inserisce una coordinata ad un detrimanto joint in un frame specifico.
     void completa_angoli(int _numeroframe); // Crea e popola la map degli angoli inserita in un determinato frame.
     void popola_sequenzaangolo(); //Da richiamare una volta popolati tutta la map "sequenzaframe" e dopo aver richiamato le varie completa_angoli
-    
+    void popola_framedaanalizzare(); // Esegue tutte le funzioni popola_max_min_anogolo necessarie per popoalre le 2 MAP e inoltre popola la set di frame da analizzare.
     
     
     friend ostream& operator <<(ostream& os, const Persona& p);
