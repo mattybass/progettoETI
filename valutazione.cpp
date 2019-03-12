73147062a0f0c4ec166875b83c6ebc50a2a83948
Unknown <daniele.mattedi@studenti.unitn.it>
2019-03-12 11:22:06 +0100

    Valutazione

--- valutazione.cpp
+++ valutazione.cpp
@@ -6,8 +6,8 @@ Valutazione::Valutazione(Persona* _paz, Persona* _mod) {
 	//creo map per l'esercizio che andremo a valutare
 	//ogni esercizio prevede una map diversa, quindi bisognerebbe prevedere un inserimento iniziale
 	//nel sistema da parte del medico.
-	pesi[1] = 0.1;
-	pesi[2] = 0.5;
+	pesi[1] = pair<float,float>(0.05,0.05);
+	pesi[2] = pair<float,float>(0.35,0.15);
 	pesi[3] = 0.3;
 	pesi[5] = 0.1;
 	pesi[6] = 0.0;
--- valutazione.h
+++ valutazione.h
@@ -2,13 +2,14 @@
 #define __VALUTAZIONE_H__
 #include <map>
 #include <string>
+#include "persona.h"
 using namespace std;
 
 class Valutazione {
 private:
 	Persona* modello;
 	Persona* paziente;
-	map<int, float> pesi; //i pesi maggiori di un tot determinano quali joint andremo a prendere in considerazione
+	map<int, pair<float,float>> pesi; //i pesi maggiori di un tot determinano quali joint andremo a prendere in considerazione
 public:
 	Valutazione(Persona* _paz, Persona* _mod);
 	void percentualeEsCompletato();
