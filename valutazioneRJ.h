85393ed9a946fffb1b75bda50ec1d26ba7ce8ba2
Riccardo <riccardo.ricci-1@studenti.unitn.it>
2019-03-14 16:16:26 +0100

    Aggiunta classe di valutazione in relazione agli altri joint

---
+++ valutazioneRJ.h
@@ -0,0 +1,16 @@
+#ifndef __valutazioneRJ_h__
+#define __valutazioneRJ_h__
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class ValutazioneRJ {
+private:
+public:
+};
+
+
+#endif
